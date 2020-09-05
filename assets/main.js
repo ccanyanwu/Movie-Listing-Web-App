f3a0af1a8f6d3d4c237ca2ceceb3abae35754a9e
ccanyanwu <eme42c@gmail.com>
2020-09-05 19:13:24 +0100

    Responsiveness

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-
+//page load event listener 
 document.addEventListener('DOMContentLoaded', loadMovies);
 function loadMovies(e){
   fetch('https://api.themoviedb.org/3/trending/all/day?api_key=08a4a9a41d4afbbd8893e3e58fbfe4b2')
@@ -20,16 +20,18 @@ function loadMovies(e){
       const sixTrendings = response.results.slice(1,7);
       //loop through sixTrendings and append selected property values to the DOMContentLoaded
       sixTrendings.forEach(sixTrending => {
-        //check for the name, title, firstairdate and release date property 
-        
         const UIothers = document.querySelector('section.others');
+        //convert dates to month and year 
+        let releaseDate = `${new Date(sixTrending.release_date).toLocaleString('default', {month:'long'})} ${new Date(sixTrending.release_date).getFullYear()}` ;
+        let firstAirDate = `${new Date(sixTrending.first_air_date).toLocaleString('default', {month:'long'})} ${new Date(sixTrending.first_air_date).getFullYear()}` ;
+        //check for the name, title, firstairdate and release date property 
         if(sixTrending.name && sixTrending.first_air_date){
           UIothers.innerHTML += `
             <a href="" class="others_content">
               <img src="https://image.tmdb.org/t/p/w300/${sixTrending.backdrop_path}">
               <div>
                 <p>${sixTrending.name}</p>
-                <p>Release date :${sixTrending.first_air_date}</p>
+                <p>Released : ${firstAirDate}</p>
               </div>
             </a>`
         } else {
@@ -38,11 +40,14 @@ function loadMovies(e){
               <img src="https://image.tmdb.org/t/p/w300/${sixTrending.backdrop_path}">
               <div>
                 <p>${sixTrending.title}</p>
-                <p>Release date :${sixTrending.release_date}</p>
+                <p>Released : ${releaseDate}</p>
               </div>
             </a>`
         }
       });
+ })
+ .catch(err => {
+   alert('ensure you are connected to the internet');
  });
   e.preventDefault();
 } 
