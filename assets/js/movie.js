64e69c11b3962e7b599d37f947c87afd29d306e4
ccanyanwu <eme42c@gmail.com>
2020-09-09 14:25:16 +0100

    Search movie

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -19,4 +19,27 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 //filter event 
+const searchInput = document.querySelector('.searchTerm');
+const search = document.querySelector('.searchButton');
+searchInput.addEventListener('keydown', function(){
+  document.querySelector('section.movies').innerHTML = ''
+  searchValue = searchInput.value.toLowerCase();
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=08a4a9a41d4afbbd8893e3e58fbfe4b2&query=${searchValue}`)
+    .then(res => res.json())
+      .then(
+        res =>{
+          let allMovies = res.results;
+      //loop through the result array to display movies 
+      allMovies.forEach(movie =>{
+        
+      document.querySelector('section.movies').innerHTML += `
+      <div>
+        <img src="https://image.tmdb.org/t/p/w300/${movie.poster_path}" alt="movie poster"/>
+        <p>${movie.title}</p>
+        <a href="" >More Details </a>
+      </div>`;
+        
+      })
+        } )
+});
 
